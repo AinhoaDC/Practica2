135cc22fca471deb8df1883827c1b5903cf051f6
Ainhoa Díaz <ainhodia@ucm.es>
2023-04-20 18:59:28 +0200

    Cuarta solución a inanición mejor comentada

--- practica2_inanicion4.py
+++ practica2_inanicion4.py
@@ -28,6 +28,7 @@ TIME_PED = 5
 TIME_IN_BRIDGE_CARS = (1, 0.5) 
 TIME_IN_BRIDGE_PEDESTRIAN = (30,10)
 
+#Creamos la clase del monitor donde vamos a solucionar el problema del puente.
 class Monitor():
     def __init__(self):
         self.mutex = Lock()
@@ -48,6 +49,7 @@ class Monitor():
         self.car_waiting_north = Condition(self.mutex)
         self.ncar = Value ('i', 0)
     
+    #Las siguientes tres funciones se crean para poder utilizarla con variables de condición. En cada caso se tendrá que esperar hasta que se cumpla la indicada en cada momento:
     def are_no_pedestrian(self): 
         return self.npedestrian.value == 0 
     
@@ -57,72 +59,81 @@ class Monitor():
     def are_nobody_north(self): 
         return self.ncar_north.value == 0 
     
+    #Usaremos la siguiente función para usarlo de condición como máximo de coches esperando. Con esto tratamos la inanición, evitamos que nunca no entren los coches. 
     def max_waiting_car(self): 
         return self.ncar_waiting.value < 15
     
+    #Usaremos la siguiente función para usarlo de condición como máximo de peatones esperando. Con esto tratamos la inanición, evitamos que nunca no entren los peatones. 
     def max_waiting_pedestrian(self): 
         return self.npedestrian_waiting.value < 5 
     
+    #Usaremos la siguiente función para usarlo de condición como máximo de coches del sur esperando. Con esto tratamos la inanición, evitamos que nunca no entren los coches procedentes del sur. 
     def max_waiting_car_south (self): 
         return self.ncar_waiting_south.value < 7 
     
+    #Usaremos la siguiente función para usarlo de condición como máximo de coches del norte esperando. Con esto tratamos la inanición, evitamos que nunca no entren los coches procedentes del norte. 
     def max_waiting_car_north (self): 
         return self.ncar_waiting_north.value < 7
     
+    #La siguiente función es para cuando los coches quieren y entran al puente: 
     def wants_enter_car(self, direction: int) -> None:
         self.mutex.acquire()
         self.ncar.value += 1 
         self.ncar_waiting.value += 1
-        self.pedestrian_waiting.wait_for(self.max_waiting_pedestrian)
-        self.no_pedestrian.wait_for(self.are_no_pedestrian)
+        self.pedestrian_waiting.wait_for(self.max_waiting_pedestrian)#Esperamos hasta que se cumpla la condición de que hay menos de 5 peatones esperando. Si se supera esta cifra de peatones, los coches tienen que dejarlos pasar. 
+        self.no_pedestrian.wait_for(self.are_no_pedestrian)#Deben esperar a que no hayan peatones.
         if direction == 1:  
             self.ncar_waiting_south.value += 1
-            self.car_waiting_north.wait_for(self.max_waiting_car_north)
-            self.no_cars_north.wait_for(self.are_nobody_north)  
+            self.car_waiting_north.wait_for(self.max_waiting_car_north)#Esperamos hasta que se cumpla la condición de que hay menos de 7 coches en el norte esperando. Si se supera esta cifra de coches, los demás tienen que dejarlos pasar. 
+            self.no_cars_north.wait_for(self.are_nobody_north)#Si el coche que quiere entrar viene del sur, debe esperar a que no haya coches del norte en el puente. 
             self.ncar_south.value += 1
             self.ncar_waiting_south.value -= 1
-            self.car_waiting_south.notify_all()
+            self.car_waiting_south.notify_all()#Avisamos a todos cuando no hay coches del sur esperando.
         if direction == 0 :
             self.ncar_waiting_north.value += 1
             if self.max_waiting_car_north(): #Condición para que solo se ejecute cuando haya pocos coches del norte esperanod, asi evitamos bloqueo si hay muchos coches en ambos sentidos esperando.
-                self.car_waiting_south.wait_for(self.max_waiting_car_south)
-            self.no_cars_south.wait_for(self.are_nobody_south) 
+                self.car_waiting_south.wait_for(self.max_waiting_car_south)#Esperamos hasta que se cumpla la condición de que hay menos de 7 coches en el sur esperando. Si se supera esta cifra de coches, los demás tienen que dejarlos pasar. 
+            self.no_cars_south.wait_for(self.are_nobody_south)#Si el coche que quiere entrar viene del norte, debe esperar a que no haya coches del sur en el puente.
             self.ncar_north.value += 1
             self.ncar_waiting_north.value -= 1 
-            self.car_waiting_north.notify_all()
+            self.car_waiting_north.notify_all()#Avisamos a todos cuando no hay coches del norte esperando.
         self.ncar_waiting.value -= 1
-        self.car_waiting.notify_all()
+        self.car_waiting.notify_all()#Avisamos a todos cuando no hay coches esperando.
         self.mutex.release()
 
+    #La siguiente función te indica cuando un coche ha cruzado el puente:
     def leaves_car(self, direction: int) -> None:
         self.mutex.acquire()
         if direction == 1  :
             self.ncar_south.value -=1 
-            if self.ncar_south.value == 0:
+            if self.ncar_south.value == 0:#Esperamos a que todos los coches del sur hayan cruzado para notificar a los coches del norte y peatones.
                 self.no_cars_south.notify_all() 
         if direction ==  0:
             self.ncar_north.value -= 1 
-            if self.ncar_north.value == 0:
+            if self.ncar_north.value == 0:#Esperamos a que todos los coches del norte hayan cruzado para notificar a los coches del sur y peatones.
                 self.no_cars_north.notify_all()
         self.mutex.release()
-
+    
+    #Función para cuando los peatones quieren cruzar el puente:
     def wants_enter_pedestrian(self) -> None:
         self.mutex.acquire()
         self.npedestrian_waiting.value += 1
         if self.max_waiting_pedestrian():#Ponemos esta condición para que no se bloquee el sistema cuando hay muchos peatones y coches esperando y no se sepa cual entrar. 
-            self.car_waiting.wait_for (self.max_waiting_car)
+            self.car_waiting.wait_for (self.max_waiting_car)#Esperamos hasta que se cumpla la condición de que hay menos de 15 coches esperando. Si se supera esta cifra de coches, los peatones tienen que dejarlos pasar. 
+        #Los peatones esperan a que no hayan coches del sur y del norte cruzando el puente.
         self.no_cars_north.wait_for(self.are_nobody_north)
         self.no_cars_south.wait_for(self.are_nobody_south)
         self.npedestrian_all.value += 1
         self.npedestrian.value += 1
         self.npedestrian_waiting.value -= 1
-        self.pedestrian_waiting.notify_all()
+        self.pedestrian_waiting.notify_all()#Avisamos a todos cuando no haya peatones esperando.
         self.mutex.release()
 
+    #Función para cuando los peatones han cruzado el puente:
     def leaves_pedestrian(self) -> None:
         self.mutex.acquire()
         self.npedestrian.value -= 1 
-        if  self.npedestrian.value == 0: 
+        if  self.npedestrian.value == 0: #Se espera a que crucen todos los peatones para notificar a los coches de que pueden cruzar. 
             self.no_pedestrian.notify_all()
         self.mutex.release()
 
